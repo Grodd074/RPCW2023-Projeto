39220a745ad13b8e7bf2d542edfc108277ec5984
Private-053 <hugojbrandao@gmail.com>
2023-06-26 19:51:32 +0100

    mais forms

--- acordaosApp/routes/index.js
+++ acordaosApp/routes/index.js
@@ -76,6 +76,59 @@ router.get('/acordaos/registo/atco1', function(req, res, next) {
     res.render('atco1Form');
 });
 
+router.get('/acordaos/registo/jcons', function(req, res, next) {
+    res.render('jconsForm');
+});
+
+router.get('/acordaos/registo/jdgpj', function(req, res, next) {
+    res.render('jdgpjForm');
+});
+
+router.get('/acordaos/registo/jsta', function(req, res, next) {
+    res.render('jstaForm');
+});
+
+router.get('/acordaos/registo/jstj', function(req, res, next) {
+    res.render('jstjForm');
+});
+
+router.get('/acordaos/registo/jtca', function(req, res, next) {
+    res.render('jtcaForm');
+});
+
+router.get('/acordaos/registo/jtcampca', function(req, res, next) {
+    res.render('jtcampcaForm');
+});
+
+router.get('/acordaos/registo/jtcampct', function(req, res, next) {
+    res.render('jtcampctForm');
+});
+
+router.get('/acordaos/registo/jtcn', function(req, res, next) {
+    res.render('jtcnForm');
+});
+
+router.get('/acordaos/registo/jtrc', function(req, res, next) {
+    res.render('jtrcForm');
+});
+
+router.get('/acordaos/registo/jtre', function(req, res, next) {
+    res.render('jtreForm');
+});
+
+router.get('/acordaos/registo/jtrg', function(req, res, next) {
+    res.render('jtrgForm');
+});
+
+router.get('/acordaos/registo/jtrl', function(req, res, next) {
+    res.render('jtrlForm');
+});
+
+router.get('/acordaos/registo/jtrp', function(req, res, next) {
+    res.render('jtrpForm');
+});
+
+
 router.post('/acordaos/registo/atco1', function(req, res, next) {
     Atco.inserir(req.body)
     .then(dados => {
@@ -84,6 +137,109 @@ router.post('/acordaos/registo/atco1', function(req, res, next) {
     .catch(e => res.render('error', {error: e}))
 });
 
+router.post('/acordaos/registo/jcons', function(req, res, next) {
+    Jcons.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jdgpj', function(req, res, next) {
+    Jdgpj.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jsta', function(req, res, next) {
+    Jsta.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jstj', function(req, res, next) {
+    Jstj.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jtca', function(req, res, next) {
+    Jtca.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jtcampca', function(req, res, next) {
+    Jtcampca.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jtcampct', function(req, res, next) {
+    Jtcampct.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jtcn', function(req, res, next) {
+    Jtcn.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.post('/acordaos/registo/jtrc', function(req, res, next) {
+    Jtrc.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}));
+});
+
+router.post('/acordaos/registo/jtre', function(req, res, next) {
+    Jtre.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}));
+});
+
+router.post('/acordaos/registo/jtrg', function(req, res, next) {
+    Jtrg.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}));
+});
+
+router.post('/acordaos/registo/jtrl', function(req, res, next) {
+    Jtrl.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}));
+});
+
+router.post('/acordaos/registo/jtrp', function(req, res, next) {
+    Jtrp.inserir(req.body)
+    .then(dados => {
+        res.redirect('/' + dados.Processo)
+    })
+    .catch(e => res.render('error', {error: e}));
+});
 
 router.get('/acordaos/:IdProcesso', function(req, res, next) {
     const processoId = new mongoose.Types.ObjectId(req.params.IdProcesso)
